b08c238d77d2183a4b1ac8b2f1037227fbe95f59
saradaabura <sobaudon@muses-c>
2026-05-11 18:50:23 +0900

    あっぷでーと

--- examples/for_mpython_example.py
+++ examples/for_mpython_example.py
@@ -1,66 +1,21 @@
-﻿# 3,92と互換性あり。
-import _thread
-from machine import UART, Pin
-import time
+﻿import machine, time, gc
 from pygps2 import pygps2
-import gc
 
-# machine.freq(285_000_000)
-#For RPi Pico2
+# バッファを大きく確保
+uart = machine.UART(0, baudrate=115200, tx=machine.Pin(0), rx=machine.Pin(1), rxbuf=2048)
+gnss = pygps2(op4=False)
 
-# GNSS parser instance
-gnss = pygps2(op4=False, op5=False)
+while True:
+    if uart.any():
+        raw_bytes = uart.read() 
+        try:
+            raw_str = raw_bytes.decode('utf-8', 'ignore')
+            gnss.analyze_sentences_block(raw_str)
+        except:
+            pass
 
-# UART setting
-uart = UART(
-    0,
-    baudrate=115200,
-    tx=Pin(0),
-    rx=Pin(1),
-    timeout=1
-)
-running = True
-# reading...
-def gps_thread():
-    global running; global gnss
-    print("[GPS] Thread started")
-
-    while running:
-        raw = uart.readline()
-        if raw:
-            try:
-
-                raw = raw.replace(b'\r', b'').replace(b'\n', b'')
-                raw = raw.replace(b'/', b'')
-                data = raw.decode("utf-8", "ignore")
-
-                if data.startswith("$"):
-                    st = time.ticks_ms()
-                    print(data)
-                    gnss.analyze_sentence(data, en_gsv=True, en_gsa=True)
-                    #print(gnss.GSV)
-                    print(time.ticks_ms() - st, ",", gc.mem_free())
-                    #print(gc.mem_free())
-
-            except Exception as e:
-                print("GPS thread error:", e)
-
-    print("[GPS] Thread stopped")
-
-# Thread starts
-_thread.start_new_thread(gps_thread, ())
-
-# MAIN LOOP
-try:
-    while True:
-        rmc = gnss.RMC
-        gsv = gnss.GSV
-        if rmc:
-            print("Lat:", rmc["latitude"], "Lon:", rmc["longitude"])
-
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    print("Stopping...")
-    running = False
-    time.sleep(0.5)
+    # 3. 解析結果の利用
+    if gnss.RMC:
+        print(gnss.RMC["latitude"], gnss.RMC["longitude"])
+    gc.collect()
+    time.sleep(1)
